160fb4193bbdd4509fb89e50f8e93f71fa7d48dd
jianzhou <sujianzhou@outlook.com>
2019-10-13 11:51:15 -0400

    Initial Commit

---
+++ cppPractice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppPractice
+//
+//  Created by Jianzhou Su on 10/13/19.
+//  Copyright © 2019 Jianzhou Su. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
